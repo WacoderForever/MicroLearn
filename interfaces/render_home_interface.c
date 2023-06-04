7fa3f0e483a6e45d00679064cd05dca018d70e11
WacoderForever <sethomondi088@gmail.com>
2023-06-04 05:34:06 +0300

    gj

--- interfaces/render_home_interface.c
+++ interfaces/render_home_interface.c
@@ -3,24 +3,23 @@ struct CTextStack * render_home_interface(char *search){
 
     s->$open(s,HTML,"lang=\"en\"");
         s->open(s,HEAD);
-            s->auto$close(s,LINK,R"(rel="stylesheet" href="/static/style.css")");
-            s->open(s,TITLE);
+                s->auto$close(s,LINK,R"(rel="stylesheet" href="/static/style.css")");
                 s->segment_text(s,"MicroLearn");
             s->close(s,TITLE);
 
         s->close(s,HEAD);
         s->open(s,BODY);
-         s->auto$close(s,INPUT,R"( name="search" placeholder='Search' )");
+         s->auto$close(s,INPUT,R"( name="search" placeholder='Search......' )");
+        create_link(s,"Home","/home");
         create_link(s,"Courses","/courses");
         create_link(s,"Instructors","/instructors");
         create_link(s,"Groups","/groups");
         create_link(s,"Resources","/resources");
         create_link(s,"About","/about");
-        s->auto$close(s,BR,"");
-           s->$open(s,DIV,R"(class="center")");
-                s->open(s,H3);
+           s->open(s,DIV);
+                s->open(s,H1);
                     s->segment_text(s,"MicroLearn");
-                s->close(s,H3);
+                s->close(s,H1);
                 s->$open(s,FORM,R"(action="/home" method="POST" )");
                 s->auto$close(s,BR,"");
                 s->close(s,FORM);
